4a147f615e41db323a385013e3ad0677dc1a5652
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-02-18 18:44:11 +0100

    fix(react-formio): Fix lint issue

--- packages/react-formio/src/components/table/utils/getPageNumbers.ts
+++ packages/react-formio/src/components/table/utils/getPageNumbers.ts
@@ -1,47 +1,55 @@
-export const LEFT_PAGE = 'LEFT'
-export const RIGHT_PAGE = 'RIGHT'
+export const LEFT_PAGE = "LEFT";
+export const RIGHT_PAGE = "RIGHT";
 
-function range (from, to, step = 1) {
-  let i = from
-  const range = []
+function range(from: number, to: number, step = 1): any[] {
+  let i = from;
+  const range = [];
 
   while (i <= to) {
-    range.push(i)
-    i += step
+    range.push(i);
+    i += step;
   }
 
-  return range
+  return range;
 }
 
-export function getPageNumbers ({ currentPage, pageNeighbours = 1, totalPages }) {
-  const totalNumbers = (pageNeighbours * 2) + 3
-  const totalBlocks = totalNumbers + 2
+export function getPageNumbers({
+  currentPage,
+  pageNeighbours = 1,
+  totalPages
+}: any) {
+  const totalNumbers = pageNeighbours * 2 + 3;
+  const totalBlocks = totalNumbers + 2;
 
   if (totalPages > totalBlocks) {
-    const calculatedStartPage = Math.max(2, currentPage - pageNeighbours)
-    const calculatedEndPage = Math.min(totalPages - 1, currentPage + pageNeighbours)
-    const startPage = (calculatedStartPage === 3) ? 2 : calculatedStartPage
-    const endPage = (calculatedEndPage === (totalPages - 2)) ? (totalPages - 1) : calculatedEndPage
+    const calculatedStartPage = Math.max(2, currentPage - pageNeighbours);
+    const calculatedEndPage = Math.min(
+      totalPages - 1,
+      currentPage + pageNeighbours
+    );
+    const startPage = calculatedStartPage === 3 ? 2 : calculatedStartPage;
+    const endPage =
+      calculatedEndPage === totalPages - 2 ? totalPages - 1 : calculatedEndPage;
 
-    let pages = range(startPage, endPage)
+    let pages = range(startPage, endPage);
 
-    const hasLeftSpill = startPage > 2
-    const hasRightSpill = (totalPages - endPage) > 1
-    const spillOffset = totalNumbers - (pages.length + 1)
-    let extraPages
+    const hasLeftSpill = startPage > 2;
+    const hasRightSpill = totalPages - endPage > 1;
+    const spillOffset = totalNumbers - (pages.length + 1);
+    let extraPages;
 
     if (hasLeftSpill && !hasRightSpill) {
-      extraPages = range(startPage - spillOffset, startPage - 1)
-      pages = [LEFT_PAGE, ...extraPages, ...pages]
+      extraPages = range(startPage - spillOffset, startPage - 1);
+      pages = [LEFT_PAGE, ...extraPages, ...pages];
     } else if (!hasLeftSpill && hasRightSpill) {
-      extraPages = range(endPage + 1, endPage + spillOffset)
-      pages = [...pages, ...extraPages, RIGHT_PAGE]
+      extraPages = range(endPage + 1, endPage + spillOffset);
+      pages = [...pages, ...extraPages, RIGHT_PAGE];
     } else {
-      pages = [LEFT_PAGE, ...pages, RIGHT_PAGE]
+      pages = [LEFT_PAGE, ...pages, RIGHT_PAGE];
     }
 
-    return [1, ...pages, totalPages]
+    return [1, ...pages, totalPages];
   }
 
-  return range(1, totalPages)
+  return range(1, totalPages);
 }
