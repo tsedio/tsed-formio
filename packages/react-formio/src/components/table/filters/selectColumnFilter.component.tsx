75e2a39005e891c5205544f5a8963eb54d95775b
Jerome Bueno <jeromebueno@macbook-pro-de-jerome.home>
2022-03-25 17:14:52 +0100

    feat: compute select filter choices

--- packages/react-formio/src/components/table/filters/selectColumnFilter.component.tsx
+++ packages/react-formio/src/components/table/filters/selectColumnFilter.component.tsx
@@ -5,8 +5,11 @@ import { Select } from "../../select/select.component";
 export function SelectColumnFilter<D extends Record<string, unknown> = {}>({
   column
 }: FilterProps<D>) {
-  const { filterValue, setFilter } = column;
-  const { choices = [] } = column as any;
+  const { id, preFilteredRows, filterValue, setFilter } = column;
+
+  const choices = [...new Set(preFilteredRows.map((row) => row.values[id]))]
+    .filter((value) => value)
+    .map((value) => ({ label: value, value }));
 
   return (
     <Select
