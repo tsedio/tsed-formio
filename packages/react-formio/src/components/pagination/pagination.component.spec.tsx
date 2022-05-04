02975a4c252da5543839c3f4f354827475d9fff7
sofianeSghiouar <sofianesgh@gmail.com>
2022-05-04 16:06:44 +0200

    ci: Pagination component: added test for callback triggerring when a button is clicked

--- packages/react-formio/src/components/pagination/pagination.component.spec.tsx
+++ packages/react-formio/src/components/pagination/pagination.component.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import React from "react";
 import { Sandbox } from "./pagination.stories";
 
@@ -10,10 +10,56 @@ describe("Pagination", () => {
     const paginationBtn = queryAllByTestId("pagination-button");
     const allBtnBlocks = paginationBtn.map((bloc) => bloc.textContent);
     const select = getByTestId("select_page");
+
     expect(container).toBeInTheDocument();
     paginationBtn.forEach((child) => expect(child).toBeInTheDocument());
     expect(select).toBeInTheDocument();
     expect(allBtnBlocks[0]).toBe("Previous");
     expect(allBtnBlocks[allBtnBlocks.length - 1]).toBe("Next");
+  });
+
+  it("should call previousPage() callback", () => {
+    const previousPageSpy = jest.fn();
+    const { queryAllByTestId } = render(
+      <Sandbox previousPage={previousPageSpy} canPreviousPage={true} />
+    );
+    const paginationBtn = queryAllByTestId("pagination-button");
+    const btnPreviousPage = paginationBtn.find(
+      (btn) => btn.textContent === "Previous"
+    );
+
+    fireEvent.click(btnPreviousPage);
+
+    expect(previousPageSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should call nextPage() callback", () => {
+    const nextPageSpy = jest.fn();
+    render(
+      <Sandbox nextPage={nextPageSpy} canNextPage={true} />
+    );
+
+    fireEvent.click(screen.getByText(/Next/i));
+
+    expect(nextPageSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should call gotoPage() callback", () => {
+    const gotoPageSpy = jest.fn();
+    const { queryAllByTestId } = render(
+      <Sandbox gotoPage={gotoPageSpy} {...Sandbox.args} />
+    );
+    const paginationBtn = queryAllByTestId("pagination-button");
+    const btnPage = paginationBtn.filter(
+      (btn) => btn.textContent !== "Previous" && btn.textContent !== "Next"
+    );
+    const min = +btnPage[0].textContent;
+    const max = min + 4;
+    const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    const btn = btnPage[randomNumber];
+
+    fireEvent.click(btn);
+
+    expect(gotoPageSpy).toHaveBeenCalledTimes(1);
   });
 });
