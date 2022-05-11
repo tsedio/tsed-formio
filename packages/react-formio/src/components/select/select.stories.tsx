74311247ada580f015fc0ab4255310e67189c89c
sofianeSghiouar <sofianesgh@gmail.com>
2022-05-11 16:07:08 +0200

    ci: Select component: rebase origin master and fix merging conflicts

--- packages/react-formio/src/components/select/select.stories.tsx
+++ packages/react-formio/src/components/select/select.stories.tsx
@@ -15,7 +15,7 @@ const useValue = (args: any) => {
     value,
     onChange(name: string, value: any) {
       setValue(value);
-      args.onChange(name, value);
+      return () => args.onChange(name, value);
     }
   };
 };
