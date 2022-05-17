74e16523f1571ec216b075385a9faf01d27d3d30
sofianeSghiouar <sofianesgh@gmail.com>
2022-05-17 17:41:27 +0200

    chore: form-control, stories and tests added

--- packages/react-formio/src/components/form-control/formControl.component.tsx
+++ packages/react-formio/src/components/form-control/formControl.component.tsx
@@ -8,42 +8,52 @@ export interface FormControlProps {
   label?: string;
   className?: string;
   description?: string | React.ComponentType | any;
-  prefix?: React.ComponentType | any;
-  suffix?: React.ComponentType | any;
+  prefix?: JSX.Element | React.ComponentType | any;
+  suffix?: JSX.Element | React.ComponentType | any;
   shadow?: boolean;
 }
 
 export function FormControl({
-  children,
-  name,
-  required,
-  prefix,
-  suffix,
-  description,
-  label,
-  className
-}: React.PropsWithChildren<FormControlProps>) {
+                              children,
+                              name,
+                              required,
+                              prefix,
+                              suffix,
+                              description,
+                              label,
+                              className
+                            }: React.PropsWithChildren<FormControlProps>) {
   return (
-    <div id={`form-group-${name || ""}`} className={classnames("form-group", className)}>
+    <div data-testid={name && `form-group-${name}`} id={`form-group-${name || ""}`}
+         className={classnames("form-group", className)}>
       {label && (
-        <label htmlFor={name} className={`col-form-label ${required ? " field-required" : ""}`}>
+        <label htmlFor={name} data-testid="form-control-label"
+               className={`col-form-label ${required ? " field-required" : ""}`}>
           {label}
         </label>
       )}
       <div className={"input-group"}>
         {prefix && (
-          <div className='input-group-prepend'>
-            <span className='input-group-text'>{prefix}</span>
+          <div className="input-group-prepend">
+            <span className="input-group-text" data-testid="form-control-prefix">
+              {prefix}
+            </span>
           </div>
         )}
         {children}
         {suffix && (
-          <div className='input-group-append'>
-            <span className='input-group-text'>{suffix}</span>
+          <div className="input-group-append">
+            <span className="input-group-text" data-testid="form-control-suffix">
+              {suffix}
+            </span>
           </div>
         )}
       </div>
-      {description && <div className='form-text text-muted'>{description}</div>}
+      {description && (
+        <div data-testid="form-control-description" className="form-text text-muted">
+          {description}
+        </div>
+      )}
     </div>
   );
 }
---
+++ packages/react-formio/src/components/form-control/formControl.stories.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import {FormControl} from "./formControl.component";
+import {iconClass} from "../../utils/iconClass";
+
+export default {
+  title: "ReactFormio/FormControl",
+  component: FormControl,
+  argTypes: {
+    label: {
+      control: {
+        type: "text"
+      }
+    },
+    name: {
+      control: {
+        type: "text"
+      }
+    },
+    children: {
+      control: {
+        type: HTMLElement || HTMLCollection
+      }
+    }
+  },
+  parameters: {}
+};
+
+export const Sandbox = (args: any) => {
+  return <FormControl {...args} />;
+};
+
+Sandbox.args = {
+  label: "Label",
+  children: <input type="text" className="form-control" placeholder="placeholder"/>
+};
+
+export const WithPrefix = (args: any) => {
+  return <FormControl {...args} />;
+};
+
+WithPrefix.args = {
+  label: "Label",
+  children: <input type="text" className="form-control" placeholder="placeholder"/>,
+  prefix: <i className={iconClass(undefined, "calendar")}/>
+};
+
+export const WithSuffix = (args: any) => {
+  return <FormControl {...args} />;
+};
+
+WithSuffix.args = {
+  label: "Label",
+  children: <input type="text" className="form-control" placeholder="placeholder"/>,
+  suffix: <i className={iconClass(undefined, "calendar")}/>
+};
+
+export const WithDescription = (args: any) => {
+  return <FormControl {...args} />;
+};
+
+WithDescription.args = {
+  label: "Label",
+  children: <input type="text" className="form-control" placeholder="placeholder"/>
+};
