9081fd92315d542c5600743a25fc006cb81b8a80
sofianeSghiouar <sofianesgh@gmail.com>
2022-05-17 07:19:43 +0200

    ci: alert component, added story and tests

---
+++ packages/react-formio/src/components/alert/alert.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Alert } from "./alert.component";
+
+export default {
+  title: "ReactFormio/Alert",
+  component: Alert,
+  argTypes: {},
+  parameters: {}
+};
+
+export const Sandbox = (args: any) => {
+  return <Alert {...args} />;
+};
+
+Sandbox.args = {
+  error: "error placeholder"
+};
