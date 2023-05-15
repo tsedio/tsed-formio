20f31258e044fc94eee29c96ef0b5ed3dd922e5f
Romain Lenzotti <rom.lenzotti@gmail.com>
2023-05-15 16:51:56 +0200

    feat(react-formio): add enableDragNDrop option

--- packages/react-formio/src/components/table/table.component.tsx
+++ packages/react-formio/src/components/table/table.component.tsx
@@ -1,8 +1,8 @@
 import classnames from "classnames";
 import React, { PropsWithChildren } from "react";
 
+import { DrapNDropContainer } from "./components/dragNDropContainer";
 import { TableProps, useCustomTable } from "./hooks/useCustomTable.hook";
-
 export function Table<Data extends object = any>(props: PropsWithChildren<TableProps<Data>>) {
   const {
     className,
@@ -21,57 +21,77 @@ export function Table<Data extends object = any>(props: PropsWithChildren<TableP
     pageSizes,
     setPageSize,
     i18n,
-    children
+    enableDragNDrop,
+    children,
+    onDrag,
+    onDrop
   } = useCustomTable(props);
 
   // Render the UI for your table
   return (
-    <div className={classnames("table-group table-responsive", className)}>
-      <table
-        className={"table table-striped table-hover"}
-        {...tableInstance.getTableProps()}
-        /* style={{ marginBottom: disablePagination ? "-1px" : "0px" }} */
-      >
-        <thead>
-          {tableInstance.headerGroups.map((headerGroup, i) => (
-            <tr {...headerGroup.getHeaderGroupProps()} key={`tableInstance.headerGroups${i}`}>
-              {headerGroup.headers.map((column) => (
-                <th
-                  /* className='text-left py-2 align-top' */
-                  {...column.getHeaderProps()}
-                  key={`tableInstance.headers.column.${column.id}`}
-                >
-                  <CellHeader column={column} />
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        {!isLoading ? (
-          <tbody {...tableInstance.getTableBodyProps()}>
-            {tableInstance.page.map((row) => {
-              tableInstance.prepareRow(row);
-              return <Row<Data> onClick={onClick} row={row} key={`tableInstance.page.${row.id}`} />;
-            })}
-          </tbody>
+    <DrapNDropContainer enableDragNDrop={enableDragNDrop}>
+      <div className={classnames("table-group table-responsive", className)}>
+        <table
+          className={"table table-striped table-hover"}
+          {...tableInstance.getTableProps()}
+          /* style={{ marginBottom: disablePagination ? "-1px" : "0px" }} */
+        >
+          <thead>
+            {tableInstance.headerGroups.map((headerGroup, i) => (
+              <tr {...headerGroup.getHeaderGroupProps()} key={`tableInstance.headerGroups${i}`}>
+                {enableDragNDrop ? (
+                  <th role='columnheader' className='text-center'>
+                    <div className='table-cell-header'></div>
+                  </th>
+                ) : null}
+                {headerGroup.headers.map((column) => (
+                  <th
+                    /* className='text-left py-2 align-top' */
+                    {...column.getHeaderProps()}
+                    key={`tableInstance.headers.column.${column.id}`}
+                  >
+                    <CellHeader column={column} />
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          {!isLoading ? (
+            <tbody {...tableInstance.getTableBodyProps()}>
+              {tableInstance.page.map((row, index) => {
+                tableInstance.prepareRow(row);
+                return (
+                  <Row<Data>
+                    index={index}
+                    enableDragNDrop={enableDragNDrop}
+                    onClick={onClick}
+                    row={row}
+                    key={`tableInstance.page.${row.id}`}
+                    onDrag={onDrag}
+                    onDrop={onDrop}
+                  />
+                );
+              })}
+            </tbody>
+          ) : null}
+        </table>
+        {isLoading ? <Loader /> : null}
+        {!data.length ? <EmptyData /> : null}
+        {!isLoading && data.length && !disablePagination ? (
+          <div className={"overflow-hidden"}>
+            <Pagination
+              {...tableInstance}
+              className={"text-sm"}
+              pageIndex={pageIndex}
+              pageSize={pageSize}
+              pageSizes={pageSizes}
+              setPageSize={setPageSize}
+              i18n={i18n}
+            />
+          </div>
         ) : null}
-      </table>
-      {isLoading ? <Loader /> : null}
-      {!data.length ? <EmptyData /> : null}
-      {!isLoading && data.length && !disablePagination ? (
-        <div className={"overflow-hidden"}>
-          <Pagination
-            {...tableInstance}
-            className={"text-sm"}
-            pageIndex={pageIndex}
-            pageSize={pageSize}
-            pageSizes={pageSizes}
-            setPageSize={setPageSize}
-            i18n={i18n}
-          />
-        </div>
-      ) : null}
-      {children}
-    </div>
+        {children}
+      </div>
+    </DrapNDropContainer>
   );
 }
