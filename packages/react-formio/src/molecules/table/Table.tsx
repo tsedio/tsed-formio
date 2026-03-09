422640f64e64939cc8e9b71f1a05f2c85ede2d4d
Romain Lenzotti <rom.lenzotti@gmail.com>
2026-03-09 17:50:18 +0100

    fix: restructure Table component layout and improve table-group styles for better responsiveness and flexibility

--- packages/react-formio/src/molecules/table/Table.tsx
+++ packages/react-formio/src/molecules/table/Table.tsx
@@ -31,57 +31,59 @@ export function Table<Data extends object = JSONRecord>({
   const { pagination } = tableInstance.getState();
 
   return (
-    <div className={cx("table-group table-responsive", className)}>
-      <table className='table table-striped table-hover'>
-        <thead>
-          {tableInstance.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                const sort = header.column.getIsSorted();
-                return (
-                  <th
-                    data-testid={`head-cell-${header.id}`}
-                    key={header.id}
-                    aria-sort={sort ? (sort === "asc" ? "ascending" : "descending") : "none"}
-                  >
-                    {header.isPlaceholder ? null : <CellHeader header={header} i18n={i18n} />}
-                  </th>
-                );
-              })}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {tableInstance.getRowModel().rows.map((row) => {
-            return (
-              <tr key={row.id} data-testid={`body-row-${row.id}`}>
-                {row
-                  .getVisibleCells()
-                  .filter((cell) => !cell.column.columnDef.meta?.hidden)
-                  .map((cell) => {
-                    return (
-                      <td {...cell.column.columnDef?.meta?.cellProps} key={cell.id} data-testid={`body-cell-${cell.id}`}>
-                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                      </td>
-                    );
-                  })}
-              </tr>
-            );
-          })}
-        </tbody>
-        {enableFooter && (
-          <tfoot>
-            {tableInstance.getFooterGroups().map((footerGroup) => (
-              <tr key={footerGroup.id}>
-                {footerGroup.headers.map((header) => (
-                  <th key={header.id}>{header.isPlaceholder ? null : <CellFooter header={header} i18n={i18n} />}</th>
-                ))}
+    <div className={cx("table-group", className)}>
+      <div className='table-group-body'>
+        <table className='table table-striped table-hover'>
+          <thead>
+            {tableInstance.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  const sort = header.column.getIsSorted();
+                  return (
+                    <th
+                      data-testid={`head-cell-${header.id}`}
+                      key={header.id}
+                      aria-sort={sort ? (sort === "asc" ? "ascending" : "descending") : "none"}
+                    >
+                      {header.isPlaceholder ? null : <CellHeader header={header} i18n={i18n} />}
+                    </th>
+                  );
+                })}
               </tr>
             ))}
-          </tfoot>
-        )}
-      </table>
-      <div className={"overflow-hidden flex flex-wrap"}>
+          </thead>
+          <tbody>
+            {tableInstance.getRowModel().rows.map((row) => {
+              return (
+                <tr key={row.id} data-testid={`body-row-${row.id}`}>
+                  {row
+                    .getVisibleCells()
+                    .filter((cell) => !cell.column.columnDef.meta?.hidden)
+                    .map((cell) => {
+                      return (
+                        <td {...cell.column.columnDef?.meta?.cellProps} key={cell.id} data-testid={`body-cell-${cell.id}`}>
+                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                        </td>
+                      );
+                    })}
+                </tr>
+              );
+            })}
+          </tbody>
+          {enableFooter && (
+            <tfoot>
+              {tableInstance.getFooterGroups().map((footerGroup) => (
+                <tr key={footerGroup.id}>
+                  {footerGroup.headers.map((header) => (
+                    <th key={header.id}>{header.isPlaceholder ? null : <CellFooter header={header} i18n={i18n} />}</th>
+                  ))}
+                </tr>
+              ))}
+            </tfoot>
+          )}
+        </table>
+      </div>
+      <div className='table-group-footer'>
         {props.data.length && pagination ? (
           <Pagination
             className={"flex-1"}
