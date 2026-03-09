815b4ef090260421fcece568ea5e8e92b70da930
Romain Lenzotti <rom.lenzotti@gmail.com>
2026-03-09 09:59:07 +0100

    fix: correct EJS syntax and refine SubmissionsTable type definitions for improved clarity and type safety

--- packages/react-formio/src/organisms/table/submissions/SubmissionsTable.tsx
+++ packages/react-formio/src/organisms/table/submissions/SubmissionsTable.tsx
@@ -1,18 +1,13 @@
-import type { FormType, JSON, SubmissionType } from "../../../interfaces";
+import type { FormType, SubmissionType } from "../../../interfaces";
+import type { JSONRecord } from "../../../interfaces/JSONRecord";
 import { Table, type TableProps } from "../../../molecules/table/Table";
 import { mapFormToColumns } from "../../../molecules/table/utils/mapFormToColumns.js";
 
-export type SubmissionsTableProps<Data extends { [key: string]: JSON } = { [key: string]: JSON }> = Omit<
-  TableProps<SubmissionType<Data>>,
-  "columns"
-> & {
+export type SubmissionsTableProps<Data extends object = JSONRecord> = Omit<TableProps<SubmissionType<Data>>, "columns"> & {
   form?: FormType;
 };
 
-export function SubmissionsTable<Data extends { [key: string]: JSON } = { [key: string]: JSON }>({
-  form,
-  ...props
-}: SubmissionsTableProps<Data>) {
+export function SubmissionsTable<Data extends object = JSONRecord>({ form, ...props }: SubmissionsTableProps<Data>) {
   const columns: any[] | undefined = form && mapFormToColumns(form);
 
   return <Table {...(props as any)} columns={columns!} />;
